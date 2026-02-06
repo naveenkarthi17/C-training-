8603f9cf94d8762268c19383c2692e68c48c3d65
naveenkarthi17 <naveenkarthi1703@gmail.com>
2026-02-06 11:10:34 +0530

    Add triangle type detection functionality

---
+++ Triangle Type Detector.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int a,b,c;
+    
+    scanf("%d%d%d",&a,&b,&c);
+    
+    if(a==b&&b==c)
+        {
+        printf("Equilateral Triangle");
+        }
+    else 
+        printf("Not a valid triangle");
+    
+    
+    return 0;
+}
