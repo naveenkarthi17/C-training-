43f0c8067494fa9ba6b3aac66fc28454836b6e88
naveenkarthi17 <naveenkarthi1703@gmail.com>
2026-02-06 11:40:49 +0530

    Create Binary Pattern 6.c

---
+++ Binary Pattern 6.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+
+    
+
+    scanf("%d",&n);
+
+    for (int i = 1; i <= n; i++) {
+
+    
+
+        for (int j = n; j>=i; j--) {
+
+            if ((i + j) % 2 == 0)
+
+                printf("1");
+
+            else
+
+                printf("0");
+
+            
+
+        }
+
+        
+
+        printf("\n");
+
+    }
+
+    return 0;
+
+}
