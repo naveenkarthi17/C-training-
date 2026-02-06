7f2b2a100a6a5428aed11a950fcea53a96de3410
naveenkarthi17 <naveenkarthi1703@gmail.com>
2026-02-06 11:08:58 +0530

    Add character classifier for uppercase and digits
    Implement character classification for uppercase and digits.

---
+++ Character Classifier 1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    char ch;
+    int a;
+    
+    
+        scanf(" %c",&ch);
+    
+    
+    
+    if(ch == 'A')
+        {
+    printf("Uppercase Alphabet");
+    
+    }
+    else if(ch == '5')
+        
+        {
+        printf("Digit");
+        }
+    
+    
+    
+    return 0;
+}
