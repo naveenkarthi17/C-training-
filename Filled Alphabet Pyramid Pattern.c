9c6443e435db4ace7a78a6a807f9b53342a56417
naveenkarthi17 <naveenkarthi1703@gmail.com>
2026-02-06 11:43:37 +0530

    Add filled alphabet pyramid pattern program

---
+++ Filled Alphabet Pyramid Pattern.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+
+    int n ;
+
+scanf("%d",&n);
+
+    for (int i = 1; i <= n; i++) {
+
+        // leading spaces
+
+        for (int s = 1; s <= n - i; s++) {
+
+            printf("  ");
+
+        }
+
+        // alphabets
+
+        char ch = 'A';
+
+        for (int j = 1; j <= 2 * i-1; j++) {
+
+            printf("%c ", ch);
+
+            ch++;
+
+        }
+
+        printf("\n");
+
+    }
+
+    return 0;
+
+}
